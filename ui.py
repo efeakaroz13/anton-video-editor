1a9d041ddfe86eb9baf05b264ecc1c4432208301
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2022-08-01 02:33:20 +0300

    basic ui

---
+++ ui.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.title("Anton V1.3 - EDITOR")
+root.geometry("500x500")
+myitem = Label(root,text="Welcome")
+myitem.grid(row=0,column=0)
+root.mainloop()
+

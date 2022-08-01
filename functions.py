b77653de4a6d7cb45860cbddd85f0bef71a24bd1
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2022-08-01 12:33:24 +0300

    MP4 compress

--- functions.py
+++ functions.py
@@ -2,13 +2,15 @@ import moviepy
 import os
 
 class Editor:
-	def __init__(self,project):
-		self.project = project
-	def concatfiles(self,videos,scale=(1280,720)):
-        scalenew = str(scale[0])+":"+str(scale[1])
-		if len(videos) == 0 or len(videos) ==1:
-			raise ValueError("Not enough videos!")
-		else:
-			if len(videos)==2:
-				os.system(f"ffmpeg -y -i {videos[0]} -vf scale=1280:720 -preset slow -crf 18 {videos[1]}")
+        def __init__(self,project):
+                self.project = project
+        def concatfiles(self,videos,scale=(1280,720)):
+                scalenew = str(scale[0])+":"+str(scale[1])
+                if len(videos) == 0 or len(videos) ==1:
+                        raise ValueError("Not enough videos!")
+                else:
+                        if len(videos)==2:
+                                os.system(f"ffmpeg -y -i {videos[0]} -vf scale=1280:720 -preset slow -crf 18 {videos[1]}")
 		
+        def compress(self,filename):
+                os.system(f"ffmpeg -y -i '{filename}' -vcodec libx265 -crf 28 '{filename.replace(filename.split('/')[-1],'COMPRESSED.mp4')}'")
--- ui.py
+++ ui.py
@@ -1,10 +1,17 @@
 from tkinter import *
 from tkinter import filedialog as fd
+import functions
+from tkinter import messagebox, Tk
 
-
-
+myeditor = functions.Editor("Anton")
 
 root = Tk()
+def alert(title, message="", kind='info', hidemain=True):
+    if kind not in ('error', 'warning', 'info'):
+        raise ValueError('Unsupported alert kind.')
+
+    show_method = getattr(messagebox, 'show{}'.format(kind))
+    show_method(title, message)
 root.title("Anton V1.3 - EDITOR")
 root.geometry("500x500")
 myitem = Label(root,text="Anton V1.3 - EDITOR")
@@ -12,13 +19,19 @@ Label(text="    ").grid(row=0,column=0)
 myitem.grid(row=0,column=1)
 file1inp = Label(root,text="File 1")
 file1inp.grid(row=1,column=0)
+filesx = []
 def selectfileone():
-	file1 = fd.askopenfilename(filetypes=[("Video files","*.mp4")])
-	file1inp.config(text="Selected")
+    file1 = fd.askopenfilename(filetypes=[("Video files","*.mp4")])
+    tempfile1 = str(file1)
+    filesx.insert(0,tempfile1)
+    file1inp.config(text="Selected")
+    alert(str(filesx))
 selectfilebutton = Button(root,text="Select a file",command=selectfileone)
 selectfilebutton.grid(row=2,column=0)
 
-
-
+def compressfile():
+    myeditor.compress(filesx[0])
+compressbtn = Button(text="Compress",command=compressfile)
+compressbtn.grid(row=3,column=0)
 root.mainloop()
 
